aaf0a462de1c08d9986e33ec7370012ef89bc4c0
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2022-08-05 18:02:21 +0200

    Minimal app based on Spring Boot 2.7.2 and Spring Native 0.12.1

--- build-native-image.bash
+++ build-native-image.bash
@@ -1,25 +1,17 @@
 #!/usr/bin/env bash
 
-set -x
 set -e
-
-echo "### Java/GraalVM version:"
-java --version
 
 # Folder where the TracingAgent will write its config
 CONFIG_FOLDER=src/main/resources/META-INF/native-image
 
 # Run the tracing agent as part of the tests
 rm -r $CONFIG_FOLDER || true # Ok if the folder does not exist
-time ./gradlew clean test # 30 sec
+time ./gradlew clean test
 ls -al $CONFIG_FOLDER
 
 # Create platform specific native image
-time ./gradlew nativeCompile --no-daemon # 3 min
-
-# Create Docker native image
-time ./gradlew bootBuildImage --no-daemon # 5 min
+time ./gradlew nativeCompile --no-daemon
 
 echo "### DONE ###"
-echo "Try native image with: ./build/native/nativeCompile/myservice"
-echo "Try Docker image with: docker run --rm magnus/myservice:latest"
+echo "Try native image with: build/native/nativeCompile/myservice"
